5d4b68bdbbef3e63f873d5764638315ecc6bbd68
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2022-03-22 23:09:11 +0300

    0.3 2

--- АиСД/Общие задания/Динамическое программирование 0.3/Main.java
+++ АиСД/Общие задания/Динамическое программирование 0.3/Main.java
@@ -32,12 +32,27 @@ class Ones {
         return res;
     }
 
+    public static long power(long base) {
+        long res = 1;
+        long temp = base;
+        int exponent = MOD - 2;
+        while (exponent > 0) {
+            if (exponent % 2 != 0) {
+                res = (res * temp) % MOD;
+            }
+            temp = (temp * temp) % MOD;
+            exponent /= 2;
+
+        }
+        return res;
+    }
+
     public void solve() {
-        int res;
+        long res;
         res = fact(n);
-        res *= pow(fact(k));
+        res *= power(fact(k)) % MOD;
         res %= MOD;
-        res *= pow(fact(n - k));
+        res *= power(fact(n - k));
         res %= MOD;
 
         System.out.print(res);
@@ -50,4 +65,50 @@ public class Main {
         Ones ones = new Ones();
         ones.solve();
     }
+}/*
+import java.util.Scanner;
+
+class Ones {
+    public static int mod = 1000000007;
+
+    public static long power(long base) {
+        long res = 1;
+        long temp = base;
+        int exponent = mod - 2;
+        while (exponent > 0) {
+            if (exponent % 2 != 0) {
+                res = (res * temp) % mod;
+            }
+            temp = (temp * temp) % mod;
+            base *= base;
+            exponent /= 2;
+
+        }
+        return res;
+    }
 }
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+
+        long up = k + 1;
+        for (int i = k + 2; i <= n; i++) {
+            up = ((up % Ones.mod) * (i % Ones.mod)) % Ones.mod;
+        }
+
+        //long down = fact(n - k);
+        //down = power(down, mod - 2) % mod;
+
+        for (int i = 1; i <= n - k; i++) {
+            up = (up * Ones.power(i)) % Ones.mod;
+        }
+
+        System.out.println(up);
+
+        //System.out.println((up * down) % mod);
+    }
+}
+*/
