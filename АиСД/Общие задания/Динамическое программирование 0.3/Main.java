b0107bcb4772c2e0796de25b4b3476dcb3bc1c44
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2022-03-23 00:02:52 +0300

    0.3 3

--- АиСД/Общие задания/Динамическое программирование 0.3/Main.java
+++ АиСД/Общие задания/Динамическое программирование 0.3/Main.java
@@ -22,27 +22,18 @@ class Ones {
         return res;
     }
 
-    private int pow(int num) {
+    private long pow(long num) {
         num %= MOD;
-        int res = num;
-        for (int i = 1; i < MOD - 1; i++) {
-            res *= num;
-            res %= MOD;
-        }
-        return res;
-    }
-
-    public static long power(long base) {
+        long pow = MOD - 2;
         long res = 1;
-        long temp = base;
-        int exponent = MOD - 2;
-        while (exponent > 0) {
-            if (exponent % 2 != 0) {
-                res = (res * temp) % MOD;
+        while(pow > 0) {
+            if (pow % 2 == 1) {
+                res *= num;
+                res %= MOD;
             }
-            temp = (temp * temp) % MOD;
-            exponent /= 2;
-
+            num *= num;
+            num %= MOD;
+            pow /= 2;
         }
         return res;
     }
@@ -50,9 +41,9 @@ class Ones {
     public void solve() {
         long res;
         res = fact(n);
-        res *= power(fact(k)) % MOD;
+        res *= pow(fact(k));
         res %= MOD;
-        res *= power(fact(n - k));
+        res *= pow(fact(n - k));
         res %= MOD;
 
         System.out.print(res);
@@ -65,50 +56,4 @@ public class Main {
         Ones ones = new Ones();
         ones.solve();
     }
-}/*
-import java.util.Scanner;
-
-class Ones {
-    public static int mod = 1000000007;
-
-    public static long power(long base) {
-        long res = 1;
-        long temp = base;
-        int exponent = mod - 2;
-        while (exponent > 0) {
-            if (exponent % 2 != 0) {
-                res = (res * temp) % mod;
-            }
-            temp = (temp * temp) % mod;
-            base *= base;
-            exponent /= 2;
-
-        }
-        return res;
-    }
 }
-public class Main {
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int k = scanner.nextInt();
-
-        long up = k + 1;
-        for (int i = k + 2; i <= n; i++) {
-            up = ((up % Ones.mod) * (i % Ones.mod)) % Ones.mod;
-        }
-
-        //long down = fact(n - k);
-        //down = power(down, mod - 2) % mod;
-
-        for (int i = 1; i <= n - k; i++) {
-            up = (up * Ones.power(i)) % Ones.mod;
-        }
-
-        System.out.println(up);
-
-        //System.out.println((up * down) % mod);
-    }
-}
-*/
