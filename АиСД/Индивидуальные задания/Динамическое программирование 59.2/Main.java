2f7ab591a8d2b9846a0b7ae26e1dab042b41b831
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2022-04-07 20:44:41 +0300

    59.2 2

--- АиСД/Индивидуальные задания/Динамическое программирование 59.2/Main.java
+++ АиСД/Индивидуальные задания/Динамическое программирование 59.2/Main.java
@@ -3,6 +3,9 @@ import java.io.*;
 class Report {
     private final int[] a;
     private final int n;
+    private int k;
+    private int[][] sol;
+    private final static int MAX_VALUE = 100001;
 
     public Report() throws IOException {
         StreamTokenizer st = new StreamTokenizer(new BufferedReader(new FileReader("report.in")));
@@ -13,6 +16,40 @@ class Report {
             st.nextToken();
             a[i] = (int) st.nval;
         }
+        sol = new int[((n - 1) / 2) + 1][2];
+    }
+
+    public void formSol() {
+        sol[0][0] = findMax(MAX_VALUE);
+        sol[0][1] = sol[0][0];
+        for(k = 1; k <= (n - 1) / 2; k++) {
+            int x = findMax(a[sol[k - 1][1]]);
+            if(x == -1) {
+                break;
+            }
+            sol[k][0] = x;
+
+            x = findMax(a[x]);
+            if(x == -1) {
+                break;
+            }
+            sol[k][1] = x;
+        }
+        k--;
+    }
+
+    private int findMax(int lim) {
+        int max = -1;
+        for(int i = 0; i < n; i++) {
+            if(a[i] < lim && (max == -1 || a[i] > a[max])) {
+                max = i;
+            }
+        }
+        return max;
+    }
+
+    public void out() throws IOException {
+
     }
 }
 
@@ -20,5 +57,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Report myRep = new Report();
+        myRep.formSol();
+        myRep.out();
     }
 }
