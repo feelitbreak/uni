058c54b39139c2c25a99f8fc0347d71ccc067367
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2022-04-15 19:53:55 +0300

    59.2 6

--- АиСД/Индивидуальные задания/Динамическое программирование 59.2/Main.java
+++ АиСД/Индивидуальные задания/Динамическое программирование 59.2/Main.java
@@ -3,8 +3,7 @@ import java.io.*;
 class Report {
     private final int[] a;
     private final int n;
-    private int k;
-    private final int[][] sol;
+    private final int[][][] sol;
     private final static int MAX_VALUE = 100001;
 
     public Report() throws IOException {
@@ -16,51 +15,79 @@ class Report {
             st.nextToken();
             a[i] = (int) st.nval;
         }
-        sol = new int[((n - 1) / 2) + 1][2];
+        sol = new int[n][][];
+        sol[0] = new int[1][1];
+        sol[0][0][0] = 0;
+        if(n != 1) {
+            sol[n - 1] = new int[1][1];
+            sol[n - 1][0][0] = 0;
+        }
+        for(int i = 1; i < n - 1; i++) {
+            sol[i] = new int[3][];
+            sol[i][0] = new int[1];
+            sol[i][1] = new int[i];
+            sol[i][2] = new int[n - i - 1];
+        }
     }
 
     public void formSol() {
-        sol[0][0] = findMax(MAX_VALUE);
-        sol[0][1] = sol[0][0];
-        for(k = 1; k <= (n - 1) / 2; k++) {
-            int x = findMax(a[sol[k - 1][1]]);
-            if(x == -1) {
-                break;
-            }
-            sol[k][0] = x;
+        for(int i = 1; i < n - 1; i++) {
+            sol[i][0][0] = Math.max(lis(i), lds(i));
 
-            x = findMax(a[x]);
-            if(x == -1) {
-                break;
-            }
-            sol[k][1] = x;
         }
-        k--;
     }
 
-    private int findMax(int lim) {
-        int max = -1;
-        for(int i = 0; i < n; i++) {
-            if(a[i] < lim && (max == -1 || a[i] > a[max])) {
-                max = i;
+    private int lis(int i) {
+        int k = 0;
+        sol[i][0][k] = a[0];
+        for(int j = 1; j < i; j++) {
+            if(sol[i][0][k] < a[j]) {
+                k++;
+                sol[i][0][k] = a[j];
+            } else {
+                int s = upperBound(a[j], k + 1, i);
+                if(s == 0 || sol[i][0][s - 1] != a[j]) {
+                    sol[i][0][s] = a[j];
+                }
             }
         }
-        return max;
+        return k + 1;
+    }
+
+    private int upperBound(int x, int r, int i) {
+        int l = 0;
+        int m;
+        while(l < r) {
+            m = (l + r) / 2;
+            if (sol[i][0][m] > x) {
+                r = m;
+            } else {
+                l = m + 1;
+            }
+        }
+        return r;
+    }
+
+    private int lds(int i) {
+        int k = 0;
+        sol[i][1][k] = a[0];
+        for(int j = i + 1; j < n; j++) {
+            if(sol[i][1][k] > a[j]) {
+                k++;
+                sol[i][1][k] = a[j];
+            } else {
+                int s = upperBound(a[j], k + 1, i);
+                if(s == 0 || sol[i][1][s - 1] != a[j]) {
+                    sol[i][1][s] = a[j];
+                }
+            }
+        }
+        return k + 1;
     }
 
     public void out() throws IOException {
         FileWriter fw = new FileWriter("report.out");
         fw.write(String.valueOf(k));
-        fw.write('\n');
-        fw.write(String.valueOf(sol[k][0] + 1));
-        for(int i = k - 1; i >= 0; i--) {
-            fw.write(' ');
-            fw.write(String.valueOf(sol[i][0] + 1));
-        }
-        for(int i = 1; i <= k; i++) {
-            fw.write(' ');
-            fw.write(String.valueOf(sol[i][1] + 1));
-        }
         fw.close();
     }
 }
