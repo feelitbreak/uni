9adccf4cf3629c88bfa851a73e0c00d54f45476d
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2022-04-28 19:19:13 +0300

    59.2 17

--- АиСД/Индивидуальные задания/Динамическое программирование 59.2/Main.java
+++ АиСД/Индивидуальные задания/Динамическое программирование 59.2/Main.java
@@ -12,8 +12,8 @@ class Report {
     private final int[] ind2;
     private final int[] k1;
     private final int[] k2;
-    private final int[] res1;
-    private final int[] res2;
+    private int[] res1;
+    private int[] res2;
 
     public Report() throws IOException {
         StreamTokenizer st = new StreamTokenizer(new BufferedReader(new FileReader("report.in")));
@@ -30,14 +30,29 @@ class Report {
         ind2 = new int[n];
         k1 = new int[n];
         k2 = new int[n];
-        res1 = new int[n];
-        res2 = new int[n];
     }
 
     public void formSol() {
         lis(a, ind1, seq1, k1);
         lds();
+        for(int i = 0; i < n; i++) {
+            if(Math.min(k1[i], k2[i]) > kRes) {
+                kRes = Math.min(k1[i], k2[i]);
+                iRes = i;
+            }
+        }
+        kRes--;
+        if(kRes != 0) {
+            res1 = new int[kRes];
+            for (int i = 0, j = seq1[iRes]; i < kRes; j = ind1[j], i++) {
+                res1[i] = j;
+            }
 
+            res2 = new int[kRes];
+            for (int i = 0, j = seq2[iRes]; i < kRes; j = ind2[j], i++) {
+                res2[i] = j;
+            }
+        }
     }
 
     private void lis(int[] a, int[] ind, int[] seq, int[] k) {
@@ -106,89 +121,6 @@ class Report {
         return r;
     }
 
-    /*public void formSequence(int iRoot) {
-        Arrays.fill(ind1, -1);
-        Arrays.fill(ind2, -1);
-        int len1 = 0;
-        int len2 = 0;
-
-        int i = 0;
-        for(; i < iRoot; i++) {
-            if(a[i] < a[iRoot]) {
-                seq1[0] = i;
-                len1++;
-                break;
-            }
-        }
-        i += 1;
-        for(; i < iRoot; i++) {
-            if(a[seq1[len1 - 1]] < a[i] && a[i] < a[iRoot]) {
-                seq1[len1] = i;
-                ind1[i] = seq1[len1 - 1];
-                len1++;
-            } else if(a[i] < a[iRoot]) {
-                int s = upperBound(a[i], len1,  1);
-                if(s != len1 && (s == 0 || a[seq1[s - 1]] != a[i])) {
-                    seq1[s] = i;
-                    if (s != 0) {
-                        ind1[i] = seq1[s - 1];
-                    }
-                }
-            }
-        }
-
-
-        i = iRoot + 1;
-        for(; i < n; i++) {
-            if(a[i] < a[iRoot]) {
-                seq2[0] = i;
-                len2++;
-                break;
-            }
-        }
-        i += 1;
-        for(; i < n; i++) {
-            if(a[seq2[len2 - 1]] > a[i] && a[i] < a[iRoot]) {
-                seq2[len2] = i;
-                ind2[i] = seq2[len2 - 1];
-                len2++;
-            } else if(a[i] < a[iRoot]) {
-                int s = upperBound(a[i], len2, 2);
-                if(s != len2 && (s == 0 || a[seq2[s - 1]] != a[i])) {
-                    seq2[s] = i;
-                    if (s != 0) {
-                        ind2[i] = seq2[s - 1];
-                    }
-                }
-            }
-        }
-
-        if(kRes < Math.min(len1, len2)) {
-            iRes = iRoot;
-            kRes = Math.min(len1, len2);
-
-            i = 0;
-            int j = seq1[len1 - 1];
-            for (int k = 0; k < len1 - kRes; k++) {
-                j = ind1[j];
-            }
-            for (; j >= 0; j = ind1[j]) {
-                res1[i] = j;
-                i++;
-            }
-
-            i = 0;
-            j = seq2[len2 - 1];
-            for (int k = 0; k < len2 - kRes; k++) {
-                j = ind2[j];
-            }
-            for (; j >= 0; j = ind2[j]) {
-                res2[i] = j;
-                i++;
-            }
-        }
-    }*/
-
     public void out() throws IOException {
         PrintWriter pw = new PrintWriter("report.out");
         pw.println(kRes);
@@ -197,7 +129,7 @@ class Report {
             pw.print(' ');
         }
         pw.print(iRes + 1);
-        for(int i = kRes - 1; i >= 0; i--) {
+        for(int i = 0; i < kRes; i++) {
             pw.print(' ');
             pw.print(res2[i] + 1);
         }
