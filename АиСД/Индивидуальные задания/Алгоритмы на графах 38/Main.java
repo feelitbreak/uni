57668887f7d9084561bedb090bd6fe21e0c6fd69
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2022-07-31 16:30:21 +0300

    38 3

--- АиСД/Индивидуальные задания/Алгоритмы на графах 38/Main.java
+++ АиСД/Индивидуальные задания/Алгоритмы на графах 38/Main.java
@@ -1,109 +1,142 @@
 import java.io.*;
-import java.util.*;
 
 class Edge {
-    private final int vGroup1;
-    private final int vGroup2;
+    private final int source;
+    private final int capacity;
+    private final int flow;
     private final int weight;
+    private final int target;
 
-    public Edge(int vGroup1, int vGroup2, int weight) {
-        this.vGroup1 = vGroup1;
-        this.vGroup2 = vGroup2;
+    public Edge(int source, int capacity, int flow, int weight, int target) {
+        this.source = source;
+        this.capacity = capacity;
+        this.flow = flow;
         this.weight = weight;
+        this.target = target;
     }
 
-    public int getVGroup1() {
-        return vGroup1;
+    public int getSource() {
+        return source;
     }
 
-    public int getVGroup2() {
-        return vGroup2;
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getFlow() {
+        return flow;
     }
 
     public int getWeight() {
         return weight;
     }
+
+    public int getTarget() {
+        return target;
+    }
 }
 
-class Graph {
-    private final int[] agesDept1;
-    private final int[] agesDept2;
+class AgeGroups {
+    private int[] group1;
+    private int[] group2;
     private static final int MIN_AGE = 18;
     private static final int MAX_AGE = 60;
+    private static final int SINGLE_ROOM_VERTEX = MAX_AGE - MIN_AGE + 1;
+    private int nVertices1 = 0;
+    private int nVertices2 = 0;
     private int n = 0;
     private int m = 0;
-    private final Edge[] edges;
-    private int dis = 0;
 
-    public Graph() throws IOException {
-        agesDept1 = new int[MAX_AGE - MIN_AGE + 1];
-        agesDept2 = new int[MAX_AGE - MIN_AGE + 1];
-        int ages1 = 0;
-        int ages2 = 0;
+    protected AgeGroups() throws IOException {
+        this.group1 = new int[MAX_AGE - MIN_AGE + 1];
+        this.group2 = new int[MAX_AGE - MIN_AGE + 2];
+        inputGroups();
 
-        StreamTokenizer st = new StreamTokenizer(new BufferedReader(new FileReader("input.txt")));
-        st.nextToken();
-        while(st.lineno() == 1) {
-            if(agesDept1[((int) st.nval) - MIN_AGE] == 0) {
-                ages1++;
-            }
-            agesDept1[((int) st.nval) - MIN_AGE]++;
-            n++;
-            st.nextToken();
-        }
-        while(st.ttype != StreamTokenizer.TT_EOF) {
-            if(agesDept2[((int) st.nval) - MIN_AGE] == 0) {
-                ages2++;
-            }
-            agesDept2[((int) st.nval) - MIN_AGE]++;
-            m++;
-            st.nextToken();
+        if (this.m > this.n)
+        {
+            switchGroups();
+            switchVertices();
         }
 
-        edges = new Edge[ages1 * ages2];
-        int k = 0;
-        for(int i = 0; i < agesDept1.length; i++) {
-            for(int j = 0; j < agesDept2.length; j++) {
-                if(agesDept1[i] > 0 && agesDept2[j] > 0) {
-                    edges[k] = new Edge(i + MIN_AGE, j + MIN_AGE, 2 * Math.abs(i - j));
-                    k++;
-                }
-            }
+        if (this.n != this.m) {
+            this.group2[SINGLE_ROOM_VERTEX] = Math.abs(this.n - this.m);
+            this.nVertices2++;
         }
     }
 
-    public void findDissatisfaction() {
-        Arrays.sort(edges, Comparator.comparingInt(Edge::getWeight));
+    protected int getPeopleInGroup1(int age) {
+        return this.group1[age - MIN_AGE];
+    }
 
-        int amount;
-        for (Edge edge : edges) {
-            amount = Math.min(agesDept1[edge.getVGroup1() - MIN_AGE], agesDept2[edge.getVGroup2() - MIN_AGE]);
-            if (amount != 0) {
-                agesDept1[edge.getVGroup1() - MIN_AGE] -= amount;
-                agesDept2[edge.getVGroup2() - MIN_AGE] -= amount;
-                dis += amount * edge.getWeight();
+    protected int getPeopleInGroup2(int age) {
+        return this.group2[age - MIN_AGE];
+    }
+
+    protected int getSingleRoomPeople() {
+        return this.group2[SINGLE_ROOM_VERTEX];
+    }
+    
+    protected int getNumberOfFlowEdges() {
+        return (this.nVertices1 * this.nVertices2 + this.nVertices1 + this.nVertices2) * 2;
+    }
+
+    private void inputGroups()  throws IOException {
+        StreamTokenizer st = new StreamTokenizer(new BufferedReader(new FileReader("input.txt")));
+        st.eolIsSignificant(true);
+        st.nextToken();
+        while (st.ttype != StreamTokenizer.TT_EOL) {
+            if (this.group1[((int) st.nval) - MIN_AGE] == 0) {
+                this.nVertices1++;
             }
+
+            this.n++;
+            this.group1[((int) st.nval) - MIN_AGE]++;
+            st.nextToken();
         }
 
-        for(int i = 0; i < agesDept1.length; i++) {
-            if(agesDept1[i] > 0) {
-                dis += agesDept1[i] * (i + MIN_AGE);
-                agesDept1[i] = 0;
+        st.nextToken();
+        while (st.ttype != StreamTokenizer.TT_EOF) {
+            if (this.group2[((int) st.nval) - MIN_AGE] == 0) {
+                this.nVertices2++;
             }
-        }
 
-        for(int i = 0; i < agesDept2.length; i++) {
-            if(agesDept2[i] > 0) {
-                dis += agesDept2[i] * (i + MIN_AGE);
-                agesDept2[i] = 0;
-            }
+            this.m++;
+            this.group2[((int) st.nval) - MIN_AGE]++;
+            st.nextToken();
         }
+    }
+    
+    private void switchGroups() {
+        int[] temp = this.group1;
+        this.group1 = this.group2;
+        this.group2 = temp;
+    }
+
+    private void switchVertices() {
+        int temp = this.nVertices1;
+        this.nVertices1 = this.nVertices2;
+        this.nVertices2 = temp;
+    }
+}
+
+class Graph extends AgeGroups {
+    private Edge[] flowEdges;
+    private int dis = 0;
+    
+    public Graph() throws IOException {
+        this.flowEdges = new Edge[this.getNumberOfFlowEdges()];
+
     }
 
     public void out() throws FileNotFoundException {
         PrintWriter pw = new PrintWriter("output.txt");
-        pw.printf("%.1f", dis / 2.0);
+        pw.printf("%.1f", this.dis / 2.0);
         pw.close();
+    }
+
+    private void buildNetwork()
+    {
+
     }
 }
 
@@ -111,7 +144,6 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Graph myGraph = new Graph();
-        myGraph.findDissatisfaction();
         myGraph.out();
     }
 }
