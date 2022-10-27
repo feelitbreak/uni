169a7fdadfa1e6a8ced57e8a123606afe504894e
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2022-10-28 01:04:38 +0300

    lab2 3

--- МЧА/2 семестр/Лабораторные/lab2/Main.java
+++ МЧА/2 семестр/Лабораторные/lab2/Main.java
@@ -75,62 +75,43 @@ class K {
     }
 }
 
-class MMQ {
-    private final static int N = 10;
+class IntegralApprox {
+    public final static int N = 10;
     private final double a;
-    private final double lambda;
-    private final double[] akFred;
-    private final double[] akVolt;
+    private final double[] akLRS;
+    private final double[] akRRS;
     private final double[] xk;
     private final double[] fk;
-    private final double[] ykFred;
-    private double[] ykVolt;
     private final double h;
 
-    public MMQ(double a, double b, double lambda) {
+    public IntegralApprox(double a, double b) {
         this.a = a;
-        this.lambda = lambda;
-        this.akFred = new double[N + 1];
-        this.akVolt = new double[N + 1];
+        this.akLRS = new double[N + 1];
+        this.akRRS = new double[N + 1];
         this.xk = new double[N + 1];
         this.fk = new double[N + 1];
-        this.ykFred = new double[N + 1];
 
         this.h = (b - this.a) / N;
         this.setXk();
         this.setFk();
+        this.setAkRRS();
+        this.setAkLRS();
     }
 
-    public void LRSFredholm() {
-        this.setAkFred();
-
-        double[][] systemMatrix;
-        systemMatrix = new double[N][N];
-        this.setSystemMatrix(systemMatrix);
-
-        double[] fkCopy = Arrays.copyOf(fk, fk.length);
-
-        double[] yk1 = Gauss.solve(systemMatrix, fkCopy);
-        System.arraycopy(yk1, 0, this.ykFred, 0, yk1.length);
-        this.ykFred[N] = this.getYkNFred();
+    public double[] getAkLRS() {
+        return akLRS;
     }
 
-    public void RRSVolterra() {
-        this.setAkVolt();
-        this.ykVolt = this.getYkVolt();
+    public double[] getAkRRS() {
+        return akRRS;
     }
 
-    public void outRes() {
-        Formatter fmt = new Formatter();
-        fmt.format("Полученное приближённое решение yk для уравнения Фредгольма:\n");
-        for (int i = 0; i < N + 1; i++) {
-            fmt.format("%.7f\n", this.ykFred[i]);
-        }
-        fmt.format("Полученное приближённое решение yk для уравнения Вольтерра:\n");
-        for (int i = 0; i < N + 1; i++) {
-            fmt.format("%.7f\n", this.ykVolt[i]);
-        }
-        System.out.println(fmt);
+    public double[] getXk() {
+        return xk;
+    }
+
+    public double[] getFk() {
+        return fk;
     }
 
     private void setXk() {
@@ -139,19 +120,19 @@ class MMQ {
         }
     }
 
-    private void setAkFred() {
+    private void setAkLRS() {
         for (int i = 0; i < N; i++) {
-            this.akFred[i] = this.h;
+            this.akLRS[i] = this.h;
         }
 
-        this.akFred[N] = 0.;
+        this.akLRS[N] = 0.;
     }
 
-    private void setAkVolt() {
-        this.akVolt[0] = 0.;
+    private void setAkRRS() {
+        this.akRRS[0] = 0.;
 
         for (int i = 1; i < N + 1; i++) {
-            this.akVolt[i] = this.h;
+            this.akRRS[i] = this.h;
         }
     }
 
@@ -160,11 +141,76 @@ class MMQ {
             this.fk[i] = F.getValue(this.xk[i]);
         }
     }
+}
+
+class MMQ {
+    private final int iN;
+    private final double lambda;
+    private final double[] akFred;
+    private final double[] akVolt;
+    private final double[] xk;
+    private final double[] fk;
+    private final double[] ykFred;
+    private final double[] ykVolt;
+
+    public MMQ(IntegralApprox ia, double lambda) {
+        this.akFred = ia.getAkRRS();
+        this.akVolt = ia.getAkLRS();
+        this.xk = ia.getXk();
+        this.fk = ia.getFk();
+        this.iN = IntegralApprox.N;
+
+        this.lambda = lambda;
+
+        this.ykFred = new double[this.iN + 1];
+        this.ykVolt = new double[this.iN + 1];
+    }
+
+    public void Fredholm() {
+        double[][] systemMatrix;
+        systemMatrix = new double[this.iN][this.iN];
+        this.setSystemMatrix(systemMatrix);
+
+        double[] fkCopy = new double[this.iN];
+        System.arraycopy(fk, 1, fkCopy, 0, fkCopy.length);
+
+        double[] yk1 = Gauss.solve(systemMatrix, fkCopy);
+        System.arraycopy(yk1, 0, this.ykFred, 1, yk1.length);
+        this.ykFred[0] = this.getYk0Fred();
+    }
+
+    public void Volterra() {
+        for (int i = 0; i < this.iN + 1; i++) {
+            for (int k = 0; k < i; k++) {
+                this.ykVolt[i] += this.akVolt[k] * K.getValue(this.xk[i], this.xk[k]) * this.ykVolt[k];
+            }
+
+            this.ykVolt[i] *= this.lambda;
+            this.ykVolt[i] += this.fk[i];
+
+            if (i != this.iN) {
+                this.ykVolt[i] /= 1. - (this.lambda * this.akVolt[i] * K.getValue(this.xk[i], this.xk[i]));
+            }
+        }
+    }
+
+    public void outRes() {
+        Formatter fmt = new Formatter();
+        fmt.format("Полученное приближённое решение yk для уравнения Фредгольма:\n");
+        for (int i = 0; i < this.iN + 1; i++) {
+            fmt.format("%.7f\n", this.ykFred[i]);
+        }
+        fmt.format("Полученное приближённое решение yk для уравнения Вольтерра:\n");
+        for (int i = 0; i < this.iN + 1; i++) {
+            fmt.format("%.7f\n", this.ykVolt[i]);
+        }
+        System.out.println(fmt);
+    }
 
     private void setSystemMatrix(double[][] matrix) {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                matrix[i][j] = - this.lambda * this.akFred[j] * K.getValue(this.xk[i], this.xk[j]);
+        for (int i = 0; i < this.iN; i++) {
+            for (int j = 0; j < this.iN; j++) {
+                matrix[i][j] = - this.lambda * this.akFred[j + 1] * K.getValue(this.xk[i + 1], this.xk[j + 1]);
                 if (i == j) {
                     matrix[i][j]++;
                 }
@@ -172,33 +218,76 @@ class MMQ {
         }
     }
 
-    private double getYkNFred() {
+    private double getYk0Fred() {
         double res = 0;
-        for (int i = 0; i < N; i++) {
-            res += this.akFred[i] * K.getValue(this.xk[N], xk[i]) * this.ykFred[i];
+        for (int i = 1; i < this.iN + 1; i++) {
+            res += this.akFred[i] * K.getValue(this.xk[0], xk[i]) * this.ykFred[i];
         }
 
         res *= this.lambda;
-        res += this.fk[N];
+        res += this.fk[0];
         return res;
     }
+}
 
-    private double[] getYkVolt() {
-        double[] res = new double[N + 1];
+class FPI {
+    private final static int N = 5;
+    private final int iN;
+    private final double lambda;
+    private final double[] akFred;
+    private final double[] akVolt;
+    private final double[] xk;
+    private final double[] fk;
+    private double[] ykFred;
+    private double[] ykVolt;
 
-        res[0] = this.fk[0];
+    public FPI(IntegralApprox ia, double lambda) {
+        this.akFred = ia.getAkRRS();
+        this.akVolt = ia.getAkLRS();
+        this.xk = ia.getXk();
+        this.fk = ia.getFk();
+        this.iN = IntegralApprox.N;
 
-        for (int i = 1; i < N + 1; i++) {
-            for (int k = 1; k < i; k++) {
-                res[i] += this.akVolt[k] * K.getValue(this.xk[i], this.xk[k]) * res[k];
-            }
+        this.lambda = lambda;
+    }
 
-            res[i] *= this.lambda;
-            res[i] += this.fk[i];
-            res[i] /= 1. - (this.lambda * this.akVolt[i] * K.getValue(this.xk[i], this.xk[i]));
+    public void Fredholm() {
+        double[] y1 = new double[this.iN + 1];
+        double[] y2 = new double[this.iN + 1];
+
+        System.arraycopy(fk, 0, y1, 0, fk.length);
+
+        for (int i = 0; i < N; i++) {
+            this.calcY2Fred(y1, y2);
+            System.arraycopy(y2, 0, y1, 0, y2.length);
         }
 
-        return res;
+        ykFred = y2;
+    }
+
+    public void Volterra() {
+
+    }
+
+    public void outRes() {
+        Formatter fmt = new Formatter();
+        fmt.format("Полученное приближённое решение yk для уравнения Фредгольма:\n");
+        for (int i = 0; i < this.iN + 1; i++) {
+            fmt.format("%.7f\n", this.ykFred[i]);
+        }
+        System.out.println(fmt);
+    }
+
+    private void calcY2Fred(double[] y1, double[] y2) {
+        for (int j = 0; j < this.iN + 1; j++) {
+            y2[j] = 0.;
+            for (int k = 1; k < this.iN + 1; k++) {
+                y2[j] += this.akFred[k] * K.getValue(this.xk[j], this.xk[k]) * y1[k];
+            }
+
+            y2[j] *= this.lambda;
+            y2[j] += fk[j];
+        }
     }
 }
 
@@ -208,11 +297,20 @@ public class Main {
     private final static double LAMBDA = 1.;
 
     public static void main(String[] args) {
+        IntegralApprox ia = new IntegralApprox(A, B);
+
         System.out.println("Задание 1:");
 
-        MMQ mmqFred = new MMQ(A, B, LAMBDA);
-        mmqFred.LRSFredholm();
-        mmqFred.RRSVolterra();
-        mmqFred.outRes();
+        MMQ mmq = new MMQ(ia, LAMBDA);
+        mmq.Fredholm();
+        mmq.Volterra();
+        mmq.outRes();
+
+        System.out.println("Задание 2:");
+
+        FPI fpi = new FPI(ia, LAMBDA);
+        fpi.Fredholm();
+        fpi.Volterra();
+        fpi.outRes();
     }
 }
