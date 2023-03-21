c76fb6b22420b2d7d0ea8debbd42547f7ad6ba6c
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2023-03-22 01:39:14 +0300

    lab1* difference operators method

--- ЧММФ/Лабораторные/lab1/Main.java
+++ ЧММФ/Лабораторные/lab1/Main.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class F {
     public static double getValue(double x) {
         return Math.pow(Math.sin(x), 2.);
@@ -20,30 +22,8 @@ class Q {
     }
 }
 
-class TridiagMatrixDiffAlgorithm {
-    private final int n;
-    private final double[] a;
-    private final double[] c;
-    private final double[] b;
-    private final double[] f;
-    private final double kap1;
-    private final double nu1;
-    private final double kap2;
-    private final double nu2;
-
-    public TridiagMatrixDiffAlgorithm
-            (int n, double[] a, double[] c, double[] b, double[] f,
-             double kap1, double nu1, double kap2, double nu2) {
-        this.n = n;
-        this.a = a;
-        this.c = c;
-        this.b = b;
-        this.f = f;
-        this.kap1 = kap1;
-        this.nu1 = nu1;
-        this.kap2 = kap2;
-        this.nu2 = nu2;
-    }
+record TridiagMatrixDiffAlgorithm(int n, double[] a, double[] c, double[] b, double[] f, double kap1, double nu1,
+                                  double kap2, double nu2) {
 
     public double[] solve() {
         double[] alpha, beta;
@@ -90,8 +70,90 @@ class TridiagMatrixDiffAlgorithm {
     }
 }
 
-class DiffOperatorsMethod {
+record DiffOperatorsMethod(int n, double h, double[] k, double[] kDx, double[] q, double[] f, double kap0, double g0,
+                           double kap1, double g1) {
 
+    public double[] getY() {
+        double[] a = getA();
+        double[] c = getC();
+        double[] b = getB();
+
+        double wavyKap0 = getWavyKap0();
+        double wavyG0 = getWavyG0();
+        double newKap1 = getNewKap1(wavyKap0);
+        double nu1 = getNu1(wavyKap0, wavyG0);
+
+        double wavyKap1 = getWavyKap1();
+        double wavyG1 = getWavyG1();
+        double newKap2 = getNewKap2(wavyKap1);
+        double nu2 = getNu2(wavyKap1, wavyG1);
+
+        TridiagMatrixDiffAlgorithm tmda = new TridiagMatrixDiffAlgorithm(n, a, c, b, f, newKap1, nu1, newKap2, nu2);
+
+        return tmda.solve();
+    }
+
+    private double[] getA() {
+        double[] res = new double[n - 1];
+
+        for (int i = 0; i < n - 1; i++) {
+            res[i] = ((k[i + 1] / h) - (kDx[i + 1] / 2.)) / h;
+        }
+
+        return res;
+    }
+
+    private double[] getC() {
+        double[] res = new double[n - 1];
+
+        for (int i = 0; i < n - 1; i++) {
+            res[i] = ((2. * k[i + 1]) / Math.pow(h, 2.)) + q[i + 1];
+        }
+
+        return res;
+    }
+
+    private double[] getB() {
+        double[] res = new double[n - 1];
+
+        for (int i = 0; i < n - 1; i++) {
+            res[i] = ((k[i + 1] / h) + (kDx[i + 1] / 2.)) / h;
+        }
+
+        return res;
+    }
+
+    private double getNewKap1(double wavyKap0) {
+        return k[0] / ((h * wavyKap0) + k[0]);
+    }
+
+    private double getNu1(double wavyKap0, double wavyG0) {
+        return (h * wavyG0) / ((h * wavyKap0) + k[0]);
+    }
+
+    private double getWavyKap0() {
+        return (kap0 * (1 - (h / 2.) * (kDx[0] / k[0]))) + ((h / 2.) * q[0]);
+    }
+
+    private double getWavyG0() {
+        return (g0 * (1 - (h / 2.) * (kDx[0] / k[0]))) + ((h / 2.) * f[0]);
+    }
+
+    private double getNewKap2(double wavyKap1) {
+        return k[n] / ((h * wavyKap1) + k[n]);
+    }
+
+    private double getNu2(double wavyKap1, double wavyG1) {
+        return (h * wavyG1) / ((h * wavyKap1) + k[n]);
+    }
+
+    private double getWavyKap1() {
+        return (kap1 * (1 + (h / 2.) * (kDx[n] / k[n]))) + ((h / 2.) * q[n]);
+    }
+
+    private double getWavyG1() {
+        return (g1 * (1 + (h / 2.) * (kDx[n] / k[n]))) + ((h / 2.) * f[n]);
+    }
 }
 
 class BoundaryValueProblem {
@@ -100,12 +162,12 @@ class BoundaryValueProblem {
     private static final double KAP1 = 1.;
     private static final double G1 = 1.;
     private static final double H = 0.1;
-    private int n;
-    private double[] x;
-    private double[] k;
-    private double[] kDx;
-    private double[] q;
-    private double[] f;
+    private final int n;
+    private final double[] x;
+    private final double[] k;
+    private final double[] kDx;
+    private final double[] q;
+    private final double[] f;
     private double[] y1;
 
     public BoundaryValueProblem() {
@@ -125,7 +187,24 @@ class BoundaryValueProblem {
     }
 
     public void solve() {
-        
+        DiffOperatorsMethod dom = new DiffOperatorsMethod(n, H, k, kDx, q, f, KAP0, G0, KAP1, G1);
+        y1 = dom.getY();
+    }
+
+    public void outRes() {
+        Formatter fmt = new Formatter();
+
+        fmt.format("\nЗадание 1. Аппроксимация разностными операторами.\n");
+        outY(fmt, y1);
+
+        System.out.println(fmt);
+    }
+
+    private void outY(Formatter fmt, double[] y) {
+        fmt.format("Полученное решение:\n");
+        for (int i = 0; i <= n; i++) {
+            fmt.format("%.7f\n", y[i]);
+        }
     }
 
     private void genX() {
@@ -163,5 +242,7 @@ public class Main {
 
     public static void main(String[] args) {
         BoundaryValueProblem bvp = new BoundaryValueProblem();
+        bvp.solve();
+        bvp.outRes();
     }
 }
