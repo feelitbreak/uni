0ae50b9c719bbeb4df66c061ffa7ac581a09a2ff
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2023-03-21 23:56:45 +0300

    lab2 difference tridiagonal matrix algorithm

--- ЧММФ/Лабораторные/lab1/Main.java
+++ ЧММФ/Лабораторные/lab1/Main.java
@@ -17,7 +17,7 @@ class Q {
 }
 
 class DiffTridiagMatrixAlgorithm {
-    private final double n;
+    private final int n;
     private final double[] a;
     private final double[] c;
     private final double[] b;
@@ -28,7 +28,7 @@ class DiffTridiagMatrixAlgorithm {
     private final double nu2;
 
     public DiffTridiagMatrixAlgorithm
-            (double n, double[] a, double[] c, double[] b, double[] f,
+            (int n, double[] a, double[] c, double[] b, double[] f,
              double kap1, double nu1, double kap2, double nu2) {
         this.n = n;
         this.a = a;
@@ -39,6 +39,50 @@ class DiffTridiagMatrixAlgorithm {
         this.nu1 = nu1;
         this.kap2 = kap2;
         this.nu2 = nu2;
+    }
+
+    public double[] solve() {
+        double[] alpha, beta;
+        alpha = genAlpha();
+        beta = genBeta(alpha);
+
+        double[] y;
+        y = genY(alpha, beta);
+
+        return y;
+    }
+
+    private double[] genAlpha() {
+        double[] res = new double[n];
+        res[0] = kap1;
+
+        for (int i = 0; i < n - 1; i++) {
+            res[i + 1] = b[i] / (c[i] - (a[i] * res[i]));
+        }
+
+        return res;
+    }
+
+    private double[] genBeta(double[] alpha) {
+        double[] res = new double[n];
+        res[0] = nu1;
+
+        for (int i = 0; i < n - 1; i++) {
+            res[i + 1] = (f[i] + (res[i] * a[i])) / (c[i] - (a[i] * alpha[i]));
+        }
+
+        return res;
+    }
+
+    private double[] genY(double[] alpha, double[] beta) {
+        double[] res = new double[n + 1];
+        res[n] = (nu2 + kap2 * beta[n - 1]) / (1. - (alpha[n - 1] * kap2));
+
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] = alpha[i] * res[i + 1] + beta[i];
+        }
+
+        return res;
     }
 }
 
